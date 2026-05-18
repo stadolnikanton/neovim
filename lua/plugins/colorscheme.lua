37c654305ca552b50526cba352bc6a822be50c37
Anton Stadolnik <stadolnikanton1@gmail.com>
2026-05-18 18:38:12 +0300

    Changed theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,22 +1,28 @@
 return {
     {
-        "ellisonleao/gruvbox.nvim",
+        "rebelot/kanagawa.nvim",
         priority = 1000,
         config = function()
-            require("gruvbox").setup({
-                terminal_colors = true,
-                undercurl = true,
-                italic = {
-                    strings = true,
-                    emphasis = true,
-                    comments = true,
-                    operators = false,
-                    folds = true,
+            require('kanagawa').setup({
+                compile = false,  -- enable compiling the colorscheme
+                undercurl = true, -- enable undercurls
+                commentStyle = { italic = true },
+                functionStyle = {},
+                keywordStyle = { italic = true },
+                statementStyle = { bold = true },
+                typeStyle = {},
+                transparent = false,   -- do not set background color
+                dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+                terminalColors = true, -- define vim.g.terminal_color_{0,17}
+                colors = {             -- add/modify theme and palette colors
+                    palette = {},
+                    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
                 },
-                contrast = "hard",
-                transparent_mode = true,
+                overrides = function(colors) -- add/modify highlights
+                    return {}
+                end,
+                theme = "wave", -- Load "wave" theme
             })
-            vim.cmd([[colorscheme gruvbox]])
         end,
     },
 }
